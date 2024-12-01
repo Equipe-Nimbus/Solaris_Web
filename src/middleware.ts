10223ac6e0701b2b3652dec6cb214495139497a3
Vinicius de Oliveira Laranjeiro <37382152+vdlaranjeiro@users.noreply.github.com>
2024-12-01 12:54:36 -0300

    refac: troquei a url de redirecionamento em casos de usuário não autenticado para uma variável de ambiente (#16)

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,13 @@ import { NextResponse, type NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
     const isAuthenticated = req.cookies.get("user");
     if (!isAuthenticated) {
-        return NextResponse.redirect("http://localhost:3000/auth/login");
+        const redirectUrl = process.env.NEXT_PUBLIC_UNAUTHENTICATED_REDIRECT_URL;
+        if (redirectUrl) {
+            return NextResponse.redirect(redirectUrl);
+        } else {
+            throw new Error("NEXT_PUBLIC_UNAUTHORIZED_REDIRECT_URL is not defined");
+        }
     }
-    
 }
 
 export const config = {
